84adcba97312779b2e6a352b00a17c01a93b6c73
DharmpreetAtwal <88208715+DharmpreetAtwal@users.noreply.github.com>
2023-12-16 14:45:18 -0500

    -renamed files .mjs

--- draw.mjs
+++ draw.mjs
@@ -74,11 +74,11 @@ class Line extends Shape {
                   "' y1='" + this.y1 +
                   "' x2='" + this.x2 +
                   "' y2='" + this.y2 + 
-                  "' style='stroke:rgb(0, 0, 255);stroke-width:2' />";
+                  "' style='stroke:rgb(255, 0, 0);stroke-width:2' />";
       }
 }
 
-//import * as Shape from "./shape.js";
+//import * as Shape from "./shape.mjs";
 
 class Draw {
       constructor() {
